794da95403ec48e8edf20c255f0cb9e0d1e2dd6f
Toru Nagai <nagato0614@gmail.com>
2024-08-13 22:13:56 +0900

    [update]chat.sh

--- chat.sh
+++ chat.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-./llama.cpp/llama-cli -m ./model/unsloth.Q4_K_M.gguf  -n 4096 --repeat_penalty 1.0 --color -i -r "User:" -f ./llama.cpp/prompts/chat-with-bob.txt
+./llama.cpp/llama-cli -m ./model/unsloth.Q4_K_M.gguf  -n 4096 --repeat_penalty 1.0 --color -i -r "User:" -f ./chat-with-bob.txt
